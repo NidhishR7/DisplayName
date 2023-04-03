0656a6d1c56f385a557b01cbbfcd1c2a38feb7e5
NidhishR7 <103636261+NidhishR7@users.noreply.github.com>
2023-04-03 22:03:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-
-    var name = prompt("Please Enter Your Name");
-    document.getElementById("displayArea").innerHTML = "Hey " + name + "!";
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    // check if username and password are valid
+    if (username === 'admin' && password === 'password') {
+      // redirect to protected page
+      window.location.href = 'protected-page.html';
+    } else {
+      alert('Login failed. Please try again.');
+    }
+  }
+  
